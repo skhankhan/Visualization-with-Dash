e8c1b7a5c9871390d1b40637366b5bf3b420ae50
Sir Khan <saadkm@live.com>
2018-12-15 21:21:34 -0500

    Added comments to app.py

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+# dash for app
 import dash
+# dash_core_components is used for graph
 import dash_core_components as dcc
+# dash_html_components library has all the html tags as its own compenents
 import dash_html_components as html
 
+# external_stylesheets used from the following link
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# app layout is defined here with dash_html_components
 app.layout = html.Div(children=[
+    
     html.H1(children='Hello World'),
+
+    html.Div(
+        html.P(children='Simple visualization using Dash python framework')
+    ),
 
     dcc.Graph(
         id='example-bar-graph',
@@ -23,6 +32,6 @@ app.layout = html.Div(children=[
         }
     )
 ])
-
+# running app on server
 if __name__ == '__main__':
     app.run_server(debug=True)
